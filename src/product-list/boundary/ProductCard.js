e5c241206bdb364f7eecdd6f5f093cb2f86c977c
Tiago Maia <tiagomvf@gmail.com>
2024-09-23 20:22:40 -0300

    Adding some styling

--- src/product-list/boundary/ProductCard.js
+++ src/product-list/boundary/ProductCard.js
@@ -5,14 +5,19 @@ import { html, render } from "lit-html";
  * @returns 
  */
 function template(product) {return  html`
-<div>
-    <image src=${product.image.desktop}></image>
-    <ul>
-        <li>${product.name}</li>
-        <li>${product.category}</li>
-        <li>${product.price}</li>
-    </ul>
-    <button>add to cart</button>
+<div class="grid columns-1">
+    <img class="rounded-md" src=${product.image.desktop}/>
+    <div class="flex justify-center">
+    <button class="w-2/3 border rounded-full border-rose-300 h-8">
+        <div class="flex justify-center text-sm font-semibold">
+        <img class="" src="/assets/images/icon-add-to-cart.svg"/>
+        Add to Cart
+</div>
+</div>
+    </button>
+    <span class="text-rose-400 text-sm font-semibold">${product.category}</span>
+    <span class="font-semibold" >${product.name}</span>
+    <span class="text-red font-semibold">$ ${product.price.toFixed(2)}</span>
 </div>
 `;
 }
