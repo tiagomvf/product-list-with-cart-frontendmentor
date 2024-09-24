311130cfc7a9c390361c35bda318066286a021c5
Tiago Maia <tiagomvf@gmail.com>
2024-09-24 11:14:48 -0300

    Positioning 'Add to Chart' button

--- src/product-list/boundary/ProductCard.js
+++ src/product-list/boundary/ProductCard.js
@@ -7,18 +7,20 @@ import { html, render } from "lit-html";
 function template(product) {return  html`
 
 <div class="grid columns-1">
-    <img class="rounded-md" src=${product.image.desktop}/>
-    <div class="flex justify-center">
-    <button class="w-2/3 max-w-2/3 min-w-fit border rounded-full border-rose-300 bg-rose-50 min-h-8 max-h-fit">
-        <div class="flex justify-center text-sm font-semibold">
+    <img class="row-span-2 rounded-md" src=${product.image.desktop}/>
+    <div class="flex justify-center h-7">
+    <button class="relative bottom-4 w-2/3 max-w-2/3 min-w-fit border rounded-full border-rose-300 bg-rose-50 min-h-8">
+      <div class="flex justify-center text-sm font-semibold">
         <img class="" src="./images/icon-add-to-cart.svg"/>
-        Add to Cart
-    </div>
+        <span>Add to Cart</span>
+      </div>
     </button>
     </div>
+    <div class="grid grid-cols-1">
     <span class="text-rose-400 text-sm font-semibold">${product.category}</span>
     <span class="font-semibold" >${product.name}</span>
     <span class="text-red font-semibold">$ ${product.price.toFixed(2)}</span>
+    <div>
 </div>
 `;
 }
