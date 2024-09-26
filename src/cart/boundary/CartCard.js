e5712bac574edf0097872957d3ab9a5aac7367e2
Tiago Maia <tiagomvf@gmail.com>
2024-09-26 13:28:27 -0300

    Show empty cart illustration

--- src/cart/boundary/CartCard.js
+++ src/cart/boundary/CartCard.js
@@ -6,6 +6,15 @@ function template(items, cartCard) {
     return html`
     <div class="bg-rose-50 rounded-lg px-6 py-4">
         <h2 class="text-xl font-bold text-red" id="title">Your Cart (${items.map(({amount}) => amount).reduce((x,y) => x+y, 0)})</h2>
+
+        ${ items?.length == 0 ?
+        html`
+        <div class="grid justify-items-center">
+        <img src="./images/illustration-empty-cart.svg">
+        <span class="text-xs font-semibold text-rose-400 items-center place-items-center">Your added items will appear here</span>
+        </div>
+        ` :
+        html`
         <ul class="text-sm py-2 grid grid-cols-1 gap-3">
         ${items.map(({amount, name, price}) => 
             html`
@@ -45,7 +54,8 @@ function template(items, cartCard) {
         </div>
         <div class="py-3">
         <button class="bg-red w-full font-semibold text-rose-50 rounded-full px-3 py-2 capitalize">confirm order</button>
-            </div>
+        </div>
+        `}
     </div>
         `
 }
