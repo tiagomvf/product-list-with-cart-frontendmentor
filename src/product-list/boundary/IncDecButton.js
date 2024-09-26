0d8785e280d64560b2e9c398b4a82d52d8733ddd
Tiago Maia <tiagomvf@gmail.com>
2024-09-26 07:39:54 -0300

    Styling IncDecButtom

--- src/product-list/boundary/IncDecButton.js
+++ src/product-list/boundary/IncDecButton.js
@@ -5,17 +5,21 @@ import { decrement, increment } from "../../cartSlice";
 import { store } from "../../store";
 
 const btTemplace = (amount) => html`
+<style>
+  @import url('../../../main.css');
+</style>
 <div id="add-to-cart" 
   class="
+   px-3
     border rounded-full border-rose-300 bg-red h-full
-    flex flex-row justify-around items-center text-rose-50
+    grid grid-cols-3 items-center place-items-center text-rose-50
   ">
-    <button id="decrement">
-      <img src="./images/icon-decrement-quantity.svg"/>
+    <button  id="decrement" class="size-3 flex justify-self-start justify-around items-center border rounded-full border-rose-50">
+      <img class="size-4/5 self-center" src="./images/icon-decrement-quantity.svg"/>
     </button>
-    <span class="font-semibold">${amount}</span>
-    <button id="increment">
-      <img src="./images/icon-increment-quantity.svg"/>
+    <span class="font-semibold text-sm">${amount}</span>
+    <button id="increment" class="size-3 flex justify-self-end justify-around items-center border rounded-full border-rose-50">
+      <img class="size-4/5" src="./images/icon-increment-quantity.svg"/>
     </button>
 </div>
 `
@@ -24,18 +28,19 @@ class IncDecButton extends HTMLElement {
 
   constructor(){
     super();
+    this.attachShadow({mode: "open"})
     store.subscribe(() => this.render());
   }
 
   render(){
     const amount = store.getState().cart.purchaseList.find(x => x.name == this.dataset.name)?.amount | 0;
-    render(btTemplace(amount), this);
+    render(btTemplace(amount), this.shadowRoot);
   }
 
   connectedCallback(){
     this.render();
-    this.querySelector('#decrement').addEventListener('click', () => store.dispatch(decrement({name:this.dataset.name})))
-    this.querySelector('#increment').addEventListener('click', () => store.dispatch(increment({name:this.dataset.name})))
+    this.shadowRoot.querySelector('#decrement').addEventListener('click', () => store.dispatch(decrement({name:this.dataset.name})))
+    this.shadowRoot.querySelector('#increment').addEventListener('click', () => store.dispatch(increment({name:this.dataset.name})))
   }
 
 }
