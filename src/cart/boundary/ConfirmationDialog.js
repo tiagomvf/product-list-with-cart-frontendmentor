0f456175d6d76e99a49dc362364d495bbbd7c62e
Tiago Maia <tiagomvf@gmail.com>
2024-10-07 21:05:55 -0300

    wip: List items at confirmation dialog

--- src/cart/boundary/ConfirmationDialog.js
+++ src/cart/boundary/ConfirmationDialog.js
@@ -2,11 +2,13 @@ import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
 import { continueShopping } from "../../shoppingPhaseSlice";
 import { store } from "../../store";
 import { clearCart } from "../../cartSlice";
+import { html, render } from "lit-html";
 
-const template = `
+const template = (list) => html`
   <style> @import "../../../main.css"; </style>
   <dialog class="rounded-lg">
   <p class=" text-red bordertext-xl">Greetings, one and all!</p>
+  ${list.map(({name, amount, price}) => html`<li>${amount} ${name}</li>`)}
   <form method="dialog">
     <button>OK</button>
   </form>
@@ -18,25 +20,30 @@ class ConfirmationDialog extends HTMLElement {
     constructor(){
         super();
         this.root = this.attachShadow({mode: "closed"})
-        this.root.innerHTML = template;
-        store.subscribe(() => {
-          const phase = store.getState().shoppingPhase.phase;
-          const dialog = this.root.querySelector("dialog")
-          
-          if(phase === "confirmation") {
-            dialog.showModal();
-          } else {
-            dialog.close()
-          }
+        store.subscribe(() => this.render());
+    }
 
-          const btn = this.root.querySelector("form button");
+    connectedCallback(){
+        this.render();
+        const btn = this.root.querySelector("form button");
           btn.addEventListener("click", () => {
             store.dispatch(continueShopping())
             store.dispatch(clearCart())
           })
-        });
     }
 
+    render() {
+      const purchaseList = store.getState().cart.purchaseList;
+      render(template(purchaseList), this.root);
+      const phase = store.getState().shoppingPhase.phase;
+      const dialog = this.root.querySelector("dialog")
+
+      if (phase === "confirmation") {
+        dialog.showModal();
+      } else {
+        dialog.close()
+      }
+    }
 
 }
 
